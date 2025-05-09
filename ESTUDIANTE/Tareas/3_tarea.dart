f8a3c372c1c9b93fe1d70ce77ae5490d21e475ef
Fernando Cando Chacon <fernandocando@hotmail.com>
2025-05-09 09:49:32 -0500

    Viernes Tarea

---
+++ ESTUDIANTE/Tareas/3_tarea.dart
@@ -0,0 +1,13 @@
+void main() {
+  Map<String, Map<String, dynamic>> restaurant = {
+    "restaurante1": {
+      "nombre": "Eliza",
+      "menu": {
+        "bebida": "lo que sea",
+        "fuerte": "lo que sea",
+        "principal": "lo que sea",
+      },
+    },
+  };
+  print(restaurant);
+}
